99711a852ecd5334eddd24f7558cc074a7475fe6
hcnh174 <nelsonhayes4@gmail.com>
2011-04-25 18:49:34 +0900

    added nextgen package

---
+++ src/main/java/org/vardb/nextgen/Main.java
@@ -0,0 +1,29 @@
+package org.vardb.nextgen;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String ... args) throws Exception
+	{
+		// read in directories and sample information
+		
+		// convert all files to fasta or at least fastq
+		
+		// trim reads using quality control
+		
+		// run velveth along with reference sequences
+		
+		// run velvetg
+		
+		// align contigs with reference sequence
+		
+		// call snps
+		
+	}
+}
--- src/main/java/org/vardb/web/ExtDirectRouterController.java
+++ src/main/java/org/vardb/web/ExtDirectRouterController.java
@@ -1,10 +1,18 @@
 package org.vardb.web;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Controller;
 
 import ch.ralscha.extdirectspring.controller.RouterController;
 
 @Controller
 public class ExtDirectRouterController extends RouterController {
+
+	public ExtDirectRouterController(ApplicationContext context,
+			ConversionService conversionService) {
+		super(context, conversionService);
+		// TODO Auto-generated constructor stub
+	}
 
 }
